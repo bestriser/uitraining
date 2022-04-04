481c261f8602e54c8708996c480f2091041421d6
YuyaYamamura <yuya.yamamura@best-rise.co.jp>
2022-04-05 07:22:16 +0900

    Initial Commit

---
+++ uitraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uitraining
+//
+//  Created by 山村祐也 on 2022/04/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
